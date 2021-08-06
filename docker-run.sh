949b397e41eae4f3e81dfaf9ea88b0b100eaaae2
edurso <eric@casadurso.com>
2021-08-06 16:57:12 -0400

    add another test script

--- docker-run.sh
+++ docker-run.sh
@@ -3,5 +3,5 @@
 sudo systemctl unmask docker
 sudo service docker start
 sudo service docker status
-docker run --name tfod-test -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY edurs0/tfod bash
+docker run --rm --name tfod-test -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY edurs0/tfod bash
 
---
+++ test2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import cv2
+
+if __name__ == "__main__":
+    cv2.namedWindow("fun")
