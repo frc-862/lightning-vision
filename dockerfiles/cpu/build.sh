aaa87e5ea26df28df47fc9b524d8bcbe416c716a
edurso <eric@casadurso.com>
2021-11-13 13:59:22 -0500

    switch build scripts to push since multi-arch loading is not supported

--- dockerfiles/cpu/build.sh
+++ dockerfiles/cpu/build.sh
@@ -4,4 +4,4 @@
 # docker buildx use $NEW_BUILDER
 # docker buildx inspect --bootstrap
 
-docker buildx build --platform linux/amd64,linux/arm64 -t edurs0/tfod-wkspc:latest --load .
+docker buildx build --platform linux/amd64,linux/arm64 -t edurs0/tfod-wkspc:latest --push .
