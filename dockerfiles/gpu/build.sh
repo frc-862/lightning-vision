239f96438f340ff36f0761cbc030b0618a562496
edurso <eric@casadurso.com>
2021-11-20 15:00:29 -0500

    compress to one docker repo

--- dockerfiles/gpu/build.sh
+++ dockerfiles/gpu/build.sh
@@ -4,4 +4,4 @@
 # docker buildx use $NEW_BUILDER
 # docker buildx inspect --bootstrap
 
-docker buildx build --platform linux/amd64,linux/arm64 -t edurs0/tfod-wkspc-gpu:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t edurs0/tfod-wkspc:latest-gpu --push .
