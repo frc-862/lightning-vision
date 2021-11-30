5f4f4412b218932d52cf4379f3a24889a1a4bf1b
edurso <eric@casadurso.com>
2021-11-30 09:19:32 -0500

    update dockerfiles

--- dockerfiles/jetson-old/build.sh
+++
@@ -1,7 +0,0 @@
-# Build Jetson Docker Image
-
-# docker buildx create --name $NEW_BUILDER
-# docker buildx use $NEW_BUILDER
-# docker buildx inspect --bootstrap
-
-docker buildx build --platform linux/arm64 -t edurs0/tfod-wkspc:latest-jetson --push .
