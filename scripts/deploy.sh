2d885659a903b38a8b712a06a509de3eea2e9ca7
Bobev64 <68569762+Bobev64@users.noreply.github.com>
2022-01-25 12:31:10 -0500

    PROG-142 Fixed IP address, added directory clearing before copying.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 # Usage: Copies files from the directory voidvision on the local device to the directory via scp
 # For more information on scp refer to man scp
-
-scp ../voidvision/* lightning@10.86.2.10:/lightning-vision/voidvision/
-
+ssh lightning@10.8.62.10 "rm -rf /lightning-vision/voidvision/*"
+scp ../voidvision/* lightning@10.8.62.10:/lightning-vision/voidvision/
