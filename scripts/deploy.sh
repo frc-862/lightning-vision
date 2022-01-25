584127ce5082d2f7405cd97cd9a3a8bdecaa934a
Bobev64 <68569762+Bobev64@users.noreply.github.com>
2022-01-25 10:31:13 -0500

    PROG-142 Deploy script made with scp

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+# Usage: Copies files from the directory voidvision on the local device to the directory via scp
+# For more information on scp refer to man scp
+
+scp ../voidvision/* lightning@10.86.2.10:/lightning-vision/voidvision/
+
