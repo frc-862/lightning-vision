b2a38c77dc8fd29ba280abf567a48d44cd053d81
Eric D'Urso <eric@casadurso.com>
2022-01-31 11:30:13 -0500

    PROG-161 clean up jetson setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+
+# install robotpy-cscore
 echo 'deb http://download.opensuse.org/repositories/home:/auscompgeek:/robotpy/Ubuntu_18.04_Ports/ /' | sudo tee /etc/apt/sources.list.d/home:auscompgeek:robotpy.list
-
 apt install -y curl
-
 curl -fsSL https://download.opensuse.org/repositories/home:auscompgeek:robotpy/Ubuntu_18.04_Ports/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_auscompgeek_robotpy.gpg > /dev/null
-
 apt update
-
 apt install python3-cscore
+
+# setup /etc/hosts to connect to rio
+
+# make app environment
