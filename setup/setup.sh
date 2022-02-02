7aee7365f11677726aac8b445f69280f8e8013d4
edurso <eric@casadurso.com>
2022-02-02 17:16:58 -0500

    PROG-161 work on deploy and setup scripts

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
+# check admin
+if ![ "$EUID" -eq 0 ] ; then
+    echo "you need root"
+	exit 1
+fi
+
 # install robotpy-cscore
 echo 'deb http://download.opensuse.org/repositories/home:/auscompgeek:/robotpy/Ubuntu_18.04_Ports/ /' | sudo tee /etc/apt/sources.list.d/home:auscompgeek:robotpy.list
-apt install -y curl
-curl -fsSL https://download.opensuse.org/repositories/home:auscompgeek:robotpy/Ubuntu_18.04_Ports/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_auscompgeek_robotpy.gpg > /dev/null
+wget https://download.opensuse.org/repositories/home:auscompgeek:robotpy/Ubuntu_18.04_Ports/Release.key 
+cat Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_auscompgeek_robotpy.gpg > /dev/null
 apt update
-apt install python3-cscore
+apt install -y python3-cscore
+rm Release.key
 
 # setup /etc/hosts to connect to rio
+# TODO implement
 
 # make app environment
+mkdir -p /home/lightning/voidvision/
