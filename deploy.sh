4373bb5a88581c3189fd5ec9632e6b870a6492a3
Bobev64 <68569762+Bobev64@users.noreply.github.com>
2022-02-04 20:47:45 -0500

    PROG-167 Added if statement to default without args

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 # Usage: ./deploy.sh [remote username] [remote ip]
-rsync -zravP --delete ./voidvision/ ${1}@${2}:/home/${1}/voidvision/
+if [ -z "$1" ] || [ -z "$2" ] ; then
+    echo "No command line arguments given"
+    echo "Defaulting connection to lightning@10.8.62.10"
+    rsync -zravP --delete ./voidvision/ lightning@10.8.62.10:/home/lightning/voidvision/
+else
+    echo "Connecting to $1 at $2"
+    rsync -zravP --delete ./voidvision/ ${1}@${2}:/home/${1}/voidvision/
+fi
